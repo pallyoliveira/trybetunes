4892522ad92ddfcc14f3b7e4365aa12502ae7613
Pally <francisca.bezerr@gmail.com>
2021-09-14 19:39:01 -0300

    cria os links de navegacao no cabecalho

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
 class Header extends React.Component {
   constructor() {
     super();
-
     this.state = {
       loading: false,
       user: {},
@@ -37,10 +37,14 @@ class Header extends React.Component {
 
   render() {
     const { loading } = this.state;
-
     return (
       <header data-testid="header-component">
         {loading ? <Loading /> : this.userExibe()}
+        <nav>
+          <Link to="/search" data-testid="link-to-search">Search</Link>
+          <Link to="/favorites" data-testid="link-to-favorites">Favorites</Link>
+          <Link to="/profile" data-testid="link-to-profile">Profile</Link>
+        </nav>
       </header>
     );
   }

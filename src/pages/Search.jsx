aa924201a2bdd17940c8a41470395e3f5fbf5ab2
Pally <francisca.bezerr@gmail.com>
2021-09-14 20:13:04 -0300

    utiliza a mesma funçao/logica de onChange para Login - finaliza questao 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,12 +2,42 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+    };
+    this.alteraName = this.alteraName.bind(this);
+  }
+
+  alteraName({ target }) {
+    const { value } = target;
+    this.setState({
+      name: value,
+    });
+  }
+
   render() {
+    const MIN_LENGTH = 2;
+    const { name } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <input type="text" data-testid="search-artist-input" />
-        <button type="submit" data-testid="search-artist-button">Pesquisar</button>
+        <input
+          type="text"
+          value={ name }
+          onChange={ this.alteraName }
+          data-testid="search-artist-input"
+        />
+        <button
+          type="submit"
+          // Será validado se o botão está habilitado somente se o input de nome tiver 2 ou mais caracteres
+          disabled={ name.length < MIN_LENGTH }
+          data-testid="search-artist-button"
+        >
+          Pesquisar
+
+        </button>
       </div>
     );
   }

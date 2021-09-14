be25e4f352ce41c3244fdbe3025803f39b760fcc
Pally <francisca.bezerr@gmail.com>
2021-09-14 19:22:38 -0300

    finaliza a questao 3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { getUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Header extends React.Component {
   constructor() {
     super();
 
     this.state = {
-      name: '',
       loading: false,
       user: {},
     };
+    this.userExibe = this.userExibe.bind(this);
   }
 
   componentDidMount() {
@@ -20,15 +21,27 @@ class Header extends React.Component {
     this.setState({ loading: true });
     const request = await getUser();
     this.setState({
-      user: request,
       loading: false,
-      name,
+      user: request,
     });
   }
 
-  render() {
+  userExibe() {
+    const { user } = this.state;
+    // user possui as chaves: name, email, description e image
     return (
-      <header data-testid="header-component" />
+    // ✓ Será validado se o nome da pessoa usuária está presente na tela após o retorno da API
+      <span data-testid="header-user-name">{user.name}</span>
+    );
+  }
+
+  render() {
+    const { loading } = this.state;
+
+    return (
+      <header data-testid="header-component">
+        {loading ? <Loading /> : this.userExibe()}
+      </header>
     );
   }
 }

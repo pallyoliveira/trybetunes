5b64728638aad83d65b66c9997c40211c78925f0
Pally <francisca.bezerr@gmail.com>
2021-09-14 18:46:36 -0300

    valida se a funçao getUser e chamada ao renderizar o componente

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,31 @@
 import React from 'react';
+import { getUser } from '../services/userAPI';
 
 class Header extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: '',
+      loading: false,
+      user: {},
+    };
+  }
+
+  componentDidMount() {
+    this.getApi();
+  }
+
+  async getApi() {
+    this.setState({ loading: true });
+    const request = await getUser();
+    this.setState({
+      user: request,
+      loading: false,
+      name,
+    });
+  }
+
   render() {
     return (
       <header data-testid="header-component" />

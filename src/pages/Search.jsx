1efb42ed714b08be6faac74bb95f54b7c6b8b17c
Pally <francisca.bezerr@gmail.com>
2021-09-15 17:04:19 -0300

    item 1 da questao 6 ok

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,15 +1,34 @@
 import React from 'react';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Loading from '../components/Loading';
 
 class Search extends React.Component {
   constructor() {
     super();
     this.state = {
+      loading: false,
+      album: [],
       name: '',
     };
     this.alteraName = this.alteraName.bind(this);
+    this.getAlbums = this.getAlbums.bind(this);
   }
 
+  async getAlbums() {
+    this.setState({ loading: true });
+    const { name } = this.state;
+    const disco = await searchAlbumsAPI(name); // parâmetro uma string, que deve ser o nome da banda ou artista
+    console.log(name);
+    this.setState({
+      album: disco, //  O retorno dessa função, quando encontra as informações, é um array
+      name: '',
+      loading: false,
+    });
+    console.log(disco);
+  }
+
+  // input
   alteraName({ target }) {
     const { value } = target;
     this.setState({
@@ -19,7 +38,8 @@ class Search extends React.Component {
 
   render() {
     const MIN_LENGTH = 2;
-    const { name } = this.state;
+    const { name, album, loading } = this.state;
+    //   const { artistId, artistName, collectionId, collectionName, collectionPrice, artworkUrl100, releaseDate, trackCount } = album;
     return (
       <div data-testid="page-search">
         <Header />
@@ -32,12 +52,13 @@ class Search extends React.Component {
         <button
           type="submit"
           // Será validado se o botão está habilitado somente se o input de nome tiver 2 ou mais caracteres
-          disabled={ name.length < MIN_LENGTH }
           data-testid="search-artist-button"
+          disabled={ name.length < MIN_LENGTH }
+          onClick={ this.getAlbums }
         >
           Pesquisar
-
         </button>
+        {loading ? <Loading /> : '' }
       </div>
     );
   }
